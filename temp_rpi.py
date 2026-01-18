6b3071b4d26dd1ac1f87d869ada998328bb8b746
Darkzveller <elkattoufi.youssef@gmail.com>
2026-01-18 22:23:27 +0100

    first commit

--- temp_rpi.py
+++ temp_rpi.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import subprocess
 import platform
+import time  # pour faire des pauses entre les mesures
 
 # Définir le seuil de température (en °C)
-TEMP_SEUIL = 59
+TEMP_SEUIL = 70
 
 # Fonction pour récupérer la température
 def get_temp():
@@ -26,13 +27,16 @@ def send_alert(temp):
 # Fonction principale
 def main():
     system_type = platform.system() + " " + platform.machine()
-#    print(f"Système détecté : {system_type}")
+    # print(f"Système détecté : {system_type}")
 
     temp = get_temp()
     if temp is not None:
-#        print(f"Température actuelle : {temp}°C")
+        # print(f"Température actuelle : {temp}°C")
         if temp >= TEMP_SEUIL:
             send_alert(temp)
 
 if __name__ == "__main__":
-    main()
+    while True:           # Boucle infinie
+        main()            # Vérifie la température
+        time.sleep(5)     # Pause de 5 secondes entre chaque vérification
+
